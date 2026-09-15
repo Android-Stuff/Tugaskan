e57a193bfff6aedf6cf3966fc57e42e39cea7ee0
Vincent <69299568+Android-Stuff@users.noreply.github.com>
2026-09-15 22:21:48 +0700

    renamed to tugaskan

--- script.js
+++ script.js
@@ -2,20 +2,41 @@ const inputTugas = document.getElementById("input-tugas");
 const btnTambah = document.getElementById("btn-tambah");
 const daftarTugas = document.getElementById("daftar-tugas");
 
+let idCounter = 0;
+
 btnTambah.addEventListener("click", function() {
     let teksTugas = inputTugas.value;
 
     if (teksTugas === "") {
         alert("Jadwal tidak boleh kosong!");
     } else {
-        daftarTugas.innerHTML += `
-            <li>
-                <input type="checkbox" onchange="this.nextElementSibling.classList.toggle('completed')">
-                <span>${teksTugas}</span>
-                <button class="btn-hapus" onclick="this.parentElement.remove()">Hapus</button>
-            </li>
-        `;
+        idCounter++;
+
+        daftarTugas.innerHTML = daftarTugas.innerHTML + 
+            '<li id="item-' + idCounter + '">' +
+                '<input type="checkbox" id="check-' + idCounter + '" onchange="toggleTugas(' + idCounter + ')"> ' +
+                '<span id="teks-' + idCounter + '">' + teksTugas + '</span> ' +
+                '<button onclick="hapusTugas(' + idCounter + ')">Hapus</button>' +
+            '</li>';
 
         inputTugas.value = "";
     }
 });
+
+function toggleTugas(id) {
+    const teks = document.getElementById("teks-" + id);
+    const checkbox = document.getElementById("check-" + id);
+
+    if (checkbox.checked) {
+        teks.style.textDecoration = "line-through";
+        teks.style.color = "gray";
+    } else {
+        teks.style.textDecoration = "none";
+        teks.style.color = "black";
+    }
+}
+
+function hapusTugas(id) {
+    const item = document.getElementById("item-" + id);
+    item.style.display = "none";
+}
